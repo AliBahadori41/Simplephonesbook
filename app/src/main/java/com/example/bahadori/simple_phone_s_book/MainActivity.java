6864c98adb3530714f7dcb1947b01c21ae4c14a5
AliBahadori41 <45832942+AliBahadori41@users.noreply.github.com>
2018-12-13 15:00:05 +0330

    Update Insert Activity

--- app/src/main/java/com/example/bahadori/simple_phone_s_book/MainActivity.java
+++ app/src/main/java/com/example/bahadori/simple_phone_s_book/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.bahadori.simple_phone_s_book;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
