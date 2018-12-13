c10a27ef0d73c105d95ca4f5bd3cc2bda8e3d3af
AliBahadori41 <45832942+AliBahadori41@users.noreply.github.com>
2018-12-13 16:16:05 +0330

    Update Insert Activity

--- app/src/main/java/com/example/bahadori/simple_phone_s_book/MainActivity.java
+++ app/src/main/java/com/example/bahadori/simple_phone_s_book/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.bahadori.simple_phone_s_book;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText fn;
+    EditText ln;
+    EditText num;
+    Button save;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fn=findViewById(R.id.editText2);
+        ln=findViewById(R.id.editText3);
+        num=findViewById(R.id.editText1);
+        save=findViewById(R.id.button);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                String firstname = fn.getText().toString();
+                String lastname = ln.getText().toString();
+                String number = num.getText().toString();
+
+
+
+            }
+        });
+
     }
 }
